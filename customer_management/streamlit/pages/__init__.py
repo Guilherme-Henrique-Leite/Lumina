d51db0e209e096c95fe8f1bf9b694c95fed883a3
Guilherme Albuquerque <113273576+Guilherme-Henrique-Leite@users.noreply.github.com>
2024-11-27 01:35:26 -0300

    Revert "refactor: Modularize project"

--- customer_management/streamlit/pages/__init__.py
+++ customer_management/streamlit/pages/__init__.py
@@ -2,10 +2,7 @@
 Module to outsource Streamlit pages
 """
 
-from customer_management.streamlit.pages import customer_panel
 from customer_management.streamlit.pages import overview
 from customer_management.streamlit.pages import graphic_visualization
-
 from customer_management.streamlit.pages.components.sidebar import render_sidebar
 from customer_management.streamlit.pages.components.grid_config import render_grid
-from customer_management.streamlit.pages.components.page_registry import get_pages
