640a8282c703fec9e5711608103f792dd1ca8506
Guilherme Albuquerque <guilherme.hlalbuquerque@gmail.com>
2024-11-27 04:24:20 +0000

    refactor: Changed Overview to Customers Panel and Metrics to Overview

--- customer_management/app.py
+++ customer_management/app.py
@@ -2,13 +2,9 @@
 Module to run Streamlit Page
 """
 import streamlit as st
-import os
-from pathlib import Path
 
-from customer_management.streamlit.pages import overview
 from customer_management.streamlit.pages import render_sidebar
-from customer_management.streamlit.pages import graphic_visualization
-
+from customer_management.streamlit.pages import get_pages
 
 st.set_page_config(
     page_title="Painel de Gerenciamento",
@@ -17,8 +13,5 @@ st.set_page_config(
 )
 
 page = render_sidebar()
-
-if page == "Painel Geral":
-    overview.run()
-elif page == "Visualização Gráfica":
-    graphic_visualization.run()
+pages = get_pages()
+pages[page]()
---
+++ customer_management/streamlit/pages/components/page_registry.py
@@ -0,0 +1,20 @@
+"""
+Module to register pages
+"""
+
+from customer_management.streamlit.pages import customer_panel, overview
+
+
+def get_pages():
+    """
+    Get pages dictionary with lazy imports to avoid circular dependencies
+    """
+    from customer_management.streamlit.pages import (
+        graphic_visualization
+    )
+    
+    return {
+        "Visão Geral": overview.run,
+        "Painel de Clientes": customer_panel.run,
+        "Visualização Gráfica": graphic_visualization.run,
+    }
