d3c7d4b51589d6dab4cfb043bc3417352fd7f0e6
Guilherme Albuquerque <guilherme.hlalbuquerque@gmail.com>
2024-11-27 02:10:00 +0000

    refactor: refactor pages for app.py

--- customer_management/app.py
+++ customer_management/app.py
@@ -2,10 +2,13 @@
 Module to run Streamlit Page
 """
 import streamlit as st
+import os
+from pathlib import Path
 
 from customer_management.streamlit.pages import overview
 from customer_management.streamlit.pages import render_sidebar
 from customer_management.streamlit.pages import graphic_visualization
+
 
 st.set_page_config(
     page_title="Painel de Gerenciamento",
@@ -15,7 +18,7 @@ st.set_page_config(
 
 page = render_sidebar()
 
-if page == "Visão Geral":
+if page == "Painel Geral":
     overview.run()
 elif page == "Visualização Gráfica":
     graphic_visualization.run()
