260eef30c4cb2a93e167b1726cdff22b679e8ead
Guilherme Albuquerque <guilherme.hlalbuquerque@gmail.com>
2024-12-20 13:59:57 +0000

    refactor: Refactor imports

--- customer_management/streamlit/pages/__init__.py
+++ customer_management/streamlit/pages/__init__.py
@@ -9,3 +9,4 @@ from customer_management.streamlit.pages import customer_registration
 
 from customer_management.streamlit.pages.components.sidebar import render_sidebar
 from customer_management.streamlit.pages.components.grid_config import render_grid
+from customer_management.streamlit.pages.components.page_registry import get_pages
---
+++ customer_management/streamlit/visualizations/__init__.py
@@ -0,0 +1,11 @@
+from .metrics_components import render_progress_section
+from .trend_components import create_trend_chart
+from .neighborhood_components import render_statistics
+from .metrics_components import render_metric_cards
+
+__all__ = [
+    'render_progress_section',
+    'create_trend_chart',
+    'render_statistics',
+    'render_metric_cards'
+]
