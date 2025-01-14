37f8fbf38b754cb06a25967375edef07693903da
Guilherme Albuquerque <guilherme.hlalbuquerque@gmail.com>
2025-01-14 01:29:17 +0000

    fix: fixed connection settings to neon and neon yml

--- customer_management/database/settings.py
+++ customer_management/database/settings.py
@@ -6,20 +6,13 @@ import os
 import streamlit as st
 from sqlalchemy import create_engine
 
-def get_config(key: str) -> str:
+def get_connection_string() -> str:
     if hasattr(st, "secrets"):
-        return st.secrets.get(key)
-    return os.getenv(key)
-
-DB_USER = get_config("DB_USER")
-DB_PASSWORD = get_config("DB_PASSWORD")
-DB_HOST = get_config("DB_HOST")
-DB_PORT = get_config("DB_PORT")
-DATABASE = get_config("DATABASE")
-
-HANDLER_CONNECTION = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DATABASE}?sslmode=require"
+        return st.secrets["NEON_DATABASE_URL"]
+    return os.getenv("NEON_DATABASE_URL")
 
 try:
+    HANDLER_CONNECTION = get_connection_string()
     engine = create_engine(HANDLER_CONNECTION)
 except Exception as e:
     print(f"Error connecting to database: {str(e)}")
